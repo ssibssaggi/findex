e7e03db97837981490b9d7232b3f95eb5dbefbaf
ParkSoomin <ssoomni52@gmail.com>
2026-09-06 13:57:13 +0900

    ✨feat: SpringBoot 프로젝트 초기설정

---
+++ src/main/java/com/ssibssaggi/findex/FindexApplication.java
@@ -0,0 +1,13 @@
+package com.ssibssaggi.findex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindexApplication.class, args);
+	}
+
+}
