3fad5d6895139a5ac61b82a5121e2d8d1f436a9d
ParkSoomin <ssoomni52@gmail.com>
2026-09-06 15:56:51 +0900

    🔧chore: Git Hooks 기반 코드 품질 관리 설정
    1. 코드 품질 관리를 위해, spotless/checkstyle 플러그인 의존성 추가
    2. checkstyle 실행을 위해, 커스텀 google-checks.xml 추가
    3. 커밋 전 코드 자동 포멧팅과 품질 검사를 위해, pre-commit 스크립트 추가
    4. 커밋 메시지 컨밴션 검사를 위해, commit-msg 스크립트 추가
    5. Git Hooks 적용을 위해, updateGitHook Gradle 테스크 추가

--- src/main/java/com/ssibssaggi/findex/FindexApplication.java
+++ src/main/java/com/ssibssaggi/findex/FindexApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FindexApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FindexApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FindexApplication.class, args);
+    }
 }
--- src/test/java/com/ssibssaggi/findex/FindexApplicationTests.java
+++ src/test/java/com/ssibssaggi/findex/FindexApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FindexApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
