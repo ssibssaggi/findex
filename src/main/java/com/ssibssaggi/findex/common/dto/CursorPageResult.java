e0a2ac218ff0da16328676a7661e4f0ec7aad56c
EUNSOLY <dmsthf9596@gmail.com>
2026-09-12 13:28:14 +0900

    🐛fix: 무한페이지네이션 반환타입 수정

--- src/main/java/com/ssibssaggi/findex/common/dto/CursorPageResult.java
+++ src/main/java/com/ssibssaggi/findex/common/dto/CursorPageResult.java
@@ -5,14 +5,29 @@ import java.util.function.Function;
 
 public record CursorPageResult<T>(
         List<T> content,
-        PageMeta pageMeta
+        String nextCursor,
+        Long nextIdAfter,
+        Integer size,
+        Long totalElements,
+        Boolean hasNext
 ) {
     public static <T> CursorPageResult<T> of(List<T> content, PageMeta pageMeta) {
-        return new CursorPageResult<>(content, pageMeta);
+        return new CursorPageResult<>(content,
+                pageMeta.nextCursor(),
+                pageMeta.nextIdAfter(),
+                pageMeta.size(),
+                pageMeta.totalElements(),
+                pageMeta.hasNext());
     }
 
     public <R> CursorPageResult<R> map(Function<T, R> mapper) {
         List<R> mapperContent = content.stream().map(mapper).toList();
-        return new CursorPageResult<>(mapperContent, this.pageMeta);
+        return new CursorPageResult<>(mapperContent,
+                this.nextCursor(),
+                this.nextIdAfter(),
+                this.size(),
+                this.totalElements(),
+                this.hasNext()
+        );
     }
 }
